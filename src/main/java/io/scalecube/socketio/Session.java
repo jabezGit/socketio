84172079b5b0166e3f56cfe196fafd051e559f3e
jabezGit <jinjiabinshihaoren@gmail.com>
2015-12-25 14:56:29 +0800

    添加注释
    加了一点注释

--- src/main/java/io/scalecube/socketio/Session.java
+++ src/main/java/io/scalecube/socketio/Session.java
@@ -17,6 +17,8 @@ import java.net.SocketAddress;
 import io.netty.buffer.ByteBuf;
 
 /**
+ * 当一个新的客户端要连接进来的时候，创建一个session，当成功连接之后，需要返回一个
+ * session ID 
  * When client handshake and connects to the socket.io server a
  * session is created this session handles the communication with a
  * specific client connection each client is represented with specific
@@ -26,21 +28,25 @@ import io.netty.buffer.ByteBuf;
  */
 public interface Session {
 
+  // 设置状态集合：创建，连接中，连接完成，断开中，已断开
   public enum State {
     CREATED, CONNECTING, CONNECTED, DISCONNECTING, DISCONNECTED
   }
 
   /**
    * Returns session identifier.
+   * 返回Seeion的ID
    */
   String getSessionId();
 
   /**
+   * 返回本次回话的来源
    * Returns origin header of this session.
    */
   String getOrigin();
 
   /**
+   * 返回的值：如果这个标志位为ture的话，这就意味着回话已被创建成功（返回数据交换的结果）
    * @return if this flag is true it means that session was created in result
    *         of switching transport protocol (e.g. from WebSocket to
    *         XHR-Polling).
@@ -48,6 +54,7 @@ public interface Session {
   boolean isUpgradedSession();
 
   /**
+   * 返回：协议转换类型
    * @return if that session was created in result of switching transport
    *         protocol (e.g. from WebSocket to XHR-Polling) it will return
    *         previous transport type. If this session isn't upgraded session
@@ -61,16 +68,19 @@ public interface Session {
   TransportType getTransportType();
 
   /**
+   * 返回远程的地址
    * Returns remote address of the client.
    */
   SocketAddress getRemoteAddress();
 
   /**
+   * 返回会话的状态
    * Returns session state (see {@link Session.State}).
    */
   State getState();
 
   /**
+   * 返回到客户端建立连接的本地端口
    * Returns local port to which client connection is established.
    */
   int getLocalPort();
