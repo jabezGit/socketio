ccc19b65858c490261cf762a9d812029d26fa45e
jabezGit <jinjiabinshihaoren@gmail.com>
2015-12-25 17:54:15 +0800

    理解工厂类

--- src/main/java/io/scalecube/socketio/ServerBootstrapFactory.java
+++ src/main/java/io/scalecube/socketio/ServerBootstrapFactory.java
@@ -3,6 +3,7 @@ package io.scalecube.socketio;
 import io.netty.bootstrap.ServerBootstrap;
 
 /**
+ * 这个工厂类就是用来创建ServerBootstrap服务器启动脚本
  * @author Anton Kharenko
  */
 public interface ServerBootstrapFactory {
