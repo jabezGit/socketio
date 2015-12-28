fd1aecb47c46664866d02f88e56ca131f722873c
jabezGit <jinjiabinshihaoren@gmail.com>
2015-12-28 17:21:38 +0800

    判断读到的消息是否是 FullHttpRequest

--- src/main/java/io/scalecube/socketio/pipeline/JsonpPollingHandler.java
+++ src/main/java/io/scalecube/socketio/pipeline/JsonpPollingHandler.java
@@ -47,6 +47,7 @@ public class JsonpPollingHandler extends ChannelInboundHandlerAdapter {
 
   @Override
   public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+    // 判断条件
     if (msg instanceof FullHttpRequest) {
       final FullHttpRequest req = (FullHttpRequest) msg;
       final HttpMethod requestMethod = req.getMethod();
